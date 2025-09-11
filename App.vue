a753f67e7dca2000e4fc04a9426009b5f73f7f61
xChiXinB <zhizhongwang403@gmail.com>
2025-09-11 21:16:45 +0800

    现在可以在主页上显示多个小组信息组件了

--- App.vue
+++ App.vue
@@ -1,14 +1,23 @@
 <template>
-  <Group
-    :group_name=group
-    :students=students
-  ></Group>
+  <div class="flex flex-col md:flex-row items-center md:justify-center h-auto md:min-h-screen">
+    <div class="grid grid-cols-1 gap-6 md:grid-cols-2 p-6 w-[90%] md:w-[50%]">
+      <Group v-for="(students, group) in data"
+        :group_name=group
+        :students=students
+      ></Group>
+    </div>
+    <p class="text-sm text-gray-500">Test</p>
+  </div>
 </template>
 
 <script setup>
   import Group from "./src/components/Group.vue";
   import { ref } from "vue";
 
-  const group = ref("Group");
-  const students = ref(['a', 'b', 'c']);
+  const data = ref({
+    "Group1": ['a', 'b', 'c', 'd'],
+    "Group2": ['a', 'b', 'c', 'd'],
+    "Group3": ['a', 'b', 'c', 'd'],
+    "Group4": ['a', 'b', 'c', 'd', 'e'],
+  });
 </script>
--- src/components/Group.vue
+++ src/components/Group.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="bg-blue-300 rounded-2xl hover:shadow-lg p-4">
-    <h2 class="text-4xl font-bold text-center">
+  <div class="flex flex-col bg-blue-300 rounded-2xl hover:shadow-lg p-4 items-center justify-center">
+    <h2 class="text-4xl font-bold text-center break-words">
       {{ props.group_name }}
     </h2>
     <ul class="text-gray-800 text-2xl text-center">
-      <li v-for="item in props.students" :key="item.id" class="m-1">
+      <li v-for="item in props.students" :key="item.id" class="m-1 break-words">
         {{ item }}
       </li>
     </ul>
