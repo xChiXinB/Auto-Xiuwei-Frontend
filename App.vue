c25d85a4f59cecad74fae48e252f913f6a05ea1b
xChiXinB <zhizhongwang403@gmail.com>
2025-09-11 20:08:57 +0800

    初始化小组信息展示插件

--- App.vue
+++ App.vue
@@ -1,7 +1,14 @@
 <template>
-
+  <Group
+    :group_name=group
+    :students=students
+  ></Group>
 </template>
 
 <script setup>
-  import { ref } from 'vue';
+  import Group from "./src/components/Group.vue";
+  import { ref } from "vue";
+
+  const group = ref("Group");
+  const students = ref(['a', 'b', 'c']);
 </script>
---
+++ src/components/Group.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="bg-blue-300 rounded-2xl hover:shadow-lg p-4">
+    <h2 class="text-4xl font-bold text-center">
+      {{ props.group_name }}
+    </h2>
+    <ul class="text-gray-800 text-2xl text-center">
+      <li v-for="item in props.students" :key="item.id" class="m-1">
+        {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+  import { defineProps } from "vue";
+
+  const props = defineProps({
+    group_name: {
+      type: String,
+      required: true,
+    },
+    students: {
+      type: Array,
+      required: true,
+    },
+  });
+</script>
