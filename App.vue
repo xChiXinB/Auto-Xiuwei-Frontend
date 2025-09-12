3adf6a29069a429458864186178fe417a071ec4e
xChiXinB <zhizhongwang403@gmail.com>
2025-09-12 14:03:19 +0800

    完成后端数据请求

--- App.vue
+++ App.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="flex flex-col md:flex-row items-center md:justify-center h-auto md:min-h-screen">
     <div class="grid grid-cols-1 gap-6 md:grid-cols-2 p-6 w-[90%] md:w-[50%]">
-      <Group v-for="(students, group) in data"
-        :group_name=group
+      <Group v-for="(students, group_name) in data"
+        :group_name=group_name
         :students=students
       ></Group>
     </div>
-    <p class="text-sm text-gray-500">Test</p>
   </div>
 </template>
 
@@ -14,10 +13,17 @@
   import Group from "./src/components/Group.vue";
   import { ref } from "vue";
 
-  const data = ref({
-    "Group1": ['a', 'b', 'c', 'd'],
-    "Group2": ['a', 'b', 'c', 'd'],
-    "Group3": ['a', 'b', 'c', 'd'],
-    "Group4": ['a', 'b', 'c', 'd', 'e'],
-  });
+  let data = ref();
+  const remote_server_ipv4 = '60.205.243.107';
+  const get_score_url = `http://${remote_server_ipv4}:5000/get_scores`;
+  fetch(get_score_url)
+      .then(res => {
+        if (!res.ok) {
+          throw res.error;
+        }
+        return res.json();
+      })
+      .then(json_data => {
+        data = ref(json_data);
+      })
 </script>
