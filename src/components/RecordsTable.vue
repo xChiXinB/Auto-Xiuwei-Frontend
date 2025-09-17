5dc1e0fb75bdbd5d9946a0074a247ef280f34ff3
xChiXinB <zhizhongwang403@gmail.com>
2025-09-17 12:18:47 +0800

    详细记录修复sticky问题

--- src/components/RecordsTable.vue
+++ src/components/RecordsTable.vue
@@ -1,22 +1,23 @@
 <template>
-  <div class="w-[90%] md:w-[40%] min-h-[30vh] m-8 md:h-[80vh] rounded-2xl shadow-2xl overflow-auto flex flex-col">
+  <div class="w-[90%] md:w-[40%] min-h-[30vh] m-8 md:h-[80vh] rounded-2xl shadow-2xl flex flex-col">
     <!--标题-->
-    <div class="sticky top-0 bg-blue-400 p-4 rounded-t-2xl">
+    <div class="bg-blue-400 p-4 rounded-t-2xl">
       <h1 class="text-4xl font-bold text-white text-center">
         Detailed XiuWei Records
       </h1>
     </div>
 
-    <div class="grid grid-cols-1 w-full">
-      <!--表头-->
-      <div class="sticky top-18 grid border-b border-b-gray-900" style="grid-template-columns: 2fr 1fr 1fr 2fr">
-        <div v-for="head in table_head" :key="head"
-             class="bg-blue-300 p-2 text-center text-black font-bold border-r border-r-gray-500 last-of-type:border-r-0">
-          {{ head }}
-        </div>
-      </div>
+    <!--表头-->
+    <div class="grid grid-cols-4 border-b border-b-gray-900" style="grid-template-columns: 2fr 1fr 1fr 2fr">
+      <p v-for="head in table_head" :key="head"
+         class="bg-blue-300 p-2 break-words text-center text-black font-bold border-r border-r-gray-500 last:border-r-0">
+        {{ head }}
+      </p>
+    </div>
+
+    <div class="grid grid-cols-1 w-full overflow-auto">
       <!--表格内容-->
-      <div class="grid border-b border-b-gray-900 last:border-b-0 hover:font-bold" style="grid-template-columns: 2fr 1fr 1fr 2fr"
+      <div class="grid grid-cols-4 border-b border-b-gray-900 last:border-b-0 hover:font-bold" style="grid-template-columns: 2fr 1fr 1fr 2fr"
            v-for="(row, index) in props.table_data" :key="index">
         <p class="p-1 text-gray-700 break-words text-center"
              v-for="(data, index) in row" :key="index">
