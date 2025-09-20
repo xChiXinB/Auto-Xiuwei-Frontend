1a3d115ab4f35658220e269cb6cc5740fc30af7b
xChiXinB <zhizhongwang403@gmail.com>
2025-09-20 21:11:42 +0800

    change group looking

--- src/components/Group.vue
+++ src/components/Group.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col bg-blue-300 rounded-2xl p-4 items-center justify-center
   hover:shadow-lg hover:-translate-y-2 transition duration-200 ease-in-out">
-    <h2 class="text-4xl font-bold text-center break-words">
+    <h2 class="text-4xl font-extrabold text-center break-words mb-2.5">
       {{ props.group_name }}
     </h2>
     <ul class="text-gray-800 text-2xl text-center">
