d8a1be617abdc089608cf3475c272f2d81c6882b
xChiXinB <zhizhongwang403@gmail.com>
2025-10-17 22:29:52 +0800

    优化设置页面的体验

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,28 +1,38 @@
 <template>
     <div class="w-full h-full big-bg flex flex-col items-start p-10">
-        <h1 class="text-3xl font-bold">
+        <h1 class="w-full text-3xl font-bold p-3">
             Website Color Style
         </h1>
-        <div class="w-full h-auto flex flex-row p-3 mb-10">
-            <input type="radio" name="style" value="blue" v-model="style"></input>
-            <span class="px-3 text-blue-500 text-2xl font-extrabold">Blue</span>
+        <div class="w-full h-auto flex flex-col lg:flex-row pl-3 mb-10">
+            <div class="flex flex-row items-center pb-1.5 lg:pb-0">
+                <input type="radio" name="style" value="blue" id="blue" v-model="style"></input>
+                <label for="blue" class="px-3 text-blue-500 text-2xl font-extrabold">Blue</label>
+            </div>
 
-            <input type="radio" name="style" value="pink" v-model="style"></input>
-            <span class="px-3 text-pink-500 text-2xl font-extrabold">Pink</span>
+            <div class="flex flex-row items-center pb-1.5 lg:pb-0">
+                <input type="radio" name="style" value="pink" id="pink" v-model="style"></input>
+                <label for="pink" class="px-3 text-pink-500 text-2xl font-extrabold">Pink</label>
+            </div>
 
-            <input type="radio" name="style" value="green" v-model="style"></input>
-            <span class="px-3 text-green-500 text-2xl font-extrabold">Green</span>
+            <div class="flex flex-row items-center pb-1.5 lg:pb-0">
+                <input type="radio" name="style" value="green" id="green" v-model="style"></input>
+                <label for="green" class="px-3 text-green-500 text-2xl font-extrabold">Green</label>
+            </div>
         </div>
 
-        <h1 class="text-3xl font-bold">
+        <h1 class="w-full text-3xl font-bold p-3">
             Display Order of XiuWei Records
         </h1>
-        <div class="w-full h-auto flex flex-row p-3">
-            <input type="radio" name="order" value="1" v-model="order"></input>
-            <span class="px-3 text-2xl font-extrabold">Chronological order</span>
+        <div class="w-full h-auto flex flex-col lg:flex-row pl-3 mb-10">
+            <div class="flex flex-row items-center pb-1.5 lg:pb-0">
+                <input type="radio" name="order" value="1" id="order1" v-model="order"></input>
+                <label for="order1" class="px-3 text-2xl font-extrabold">Chronological order</label>
+            </div>
 
-            <input type="radio" name="order" value="-1" v-model="order"></input>
-            <span class="px-3 text-2xl font-extrabold">Reverse chronological order</span>
+            <div class="flex flex-row items-center pb-1.5 lg:pb-0">
+                <input type="radio" name="order" value="-1" id="order-1" v-model="order"></input>
+                <label for="order-1" class="px-3 text-2xl font-extrabold">Reverse chronological order</label>
+            </div>
         </div>
     </div>
 </template>
