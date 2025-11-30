31150c4d11bd986a29f8cdb1ab4e95ea24251149
xChiXinB <zhizhongwang403@gmail.com>
2025-11-30 14:33:30 +0800

    Revert "重构matter.js的初始化"
    This reverts commit c215773b1f009461afea038c0fb005838f38cc8e.

--- src/views/Individuals.vue
+++ src/views/Individuals.vue
@@ -30,13 +30,16 @@
     import { ref, onMounted, onUnmounted, watch, nextTick, provide } from 'vue';
     import { useI18n } from 'vue-i18n';
     const { t } = useI18n();
+    import Matter from 'matter-js';
+    const { Engine, Events, Render, Runner, World, Bodies, Query } = Matter;
     import ClassTotal from '../components/Individuals/ClassTotal.vue';
     import StudentDetail from '../components/Individuals/StudentDetail.vue';
     import FetchUnsuccessful from '../components/FetchUnsuccessful.vue';
     import Loading from '../components/Loading.vue';
+    import { calculate_radii } from '../composables/Individuals/calculate_radii.js';
+    import { t2ts } from '../composables/Individuals/transactions2total_score.ts';
     import { API_route, getUsers } from '../composables/configurations.mts';
-    import { ClassTotalScore, IBTransactions, Users, usersKey } from '../composables/Individuals/interface.ts';
-    import { initMatterCanvas } from '../composables/Individuals/init_matter_canvas.ts';
+    import { BallInformation, ClassTotalScore, IBTransactions, UserId2TotalScore, Users, usersKey } from '../composables/Individuals/interface.ts';
 
     // 选中的student_id
     const selected_user_id = ref<number>(0);
@@ -77,35 +80,215 @@
 
     // StudentDetail子组件额外响应式变量
     const do_show_transactions = ref(false);
-    const clicks = ref(0);
+    const clicks = ref(0); // 球被点击的次数
 
     // matter.js变量初始化
     const min_r = Math.min(50, 0.1 * window.innerWidth);    
     const area_percentage = 0.73;
 
     const canvas = ref<HTMLCanvasElement>();
+    let w: number, h: number;
+
+    let hovered_ball: Matter.Body | null = null;
+    let selected_ball: Matter.Body | null = null;
+
+    let mousemove_event_deactivate: string | number | NodeJS.Timeout | undefined;
+    let mouse_in_canvas: boolean;
 
     function matterJsInit() {
+        // 类型收窄
         if (!(canvas.value instanceof HTMLCanvasElement)) return;
 
-        initMatterCanvas(
-            canvas.value,
-            {
-                transactions: transactions.value,
-                users: users.value,
-                min_r,
-                area_percentage,
-            },
-            (user_id: number | null) => {
-                if (user_id === null) {
-                    do_show_transactions.value = false;
-                } else {
-                    selected_user_id.value = user_id;
-                    do_show_transactions.value = true;
-                    clicks.value++;
+        const CSSStyleDecoration = window.getComputedStyle(document.body);
+        // matter.js
+        // 获取宽度需要区分大小屏幕
+        if (window.innerWidth >= 768) {
+            w = canvas.value.clientWidth;
+        } else {
+            const w_canvas = canvas.value.clientWidth;
+            const w_should = document.body.clientWidth * 0.9;
+
+            w = (Math.abs(w_canvas - w_should) < 5) ? w_canvas : w_should; // 避免正在播放的动画对canvas宽度产生的干扰影响正常宽度的读取
+        }
+        h = canvas.value.clientHeight;
+
+        // 初始化组件
+        const engine = Engine.create();
+        const render = Render.create({
+            engine: engine,
+            canvas: canvas.value,
+            options: {
+                width: w,
+                height: h,
+                wireframes: false,
+                background: CSSStyleDecoration.getPropertyValue("--big-bg"),
+            }
+        });
+        Render.run(render);
+        const runner = Runner.create();
+        Runner.run(runner, engine);
+
+        /* 遍历目标：
+        { student_id: total_score }
+        */
+        const user_id2total_score: UserId2TotalScore = {};
+        for (const user_id in transactions.value) {
+            user_id2total_score[user_id] = t2ts(transactions.value[user_id]);
+        }
+        // 为了确保接下来计算的鲁棒性，将无序对象转换为有序数组
+        const user_ids = Object.keys(user_id2total_score).map(id => Number(id));
+        const total_scores = Object.values(user_id2total_score);
+        const radii = calculate_radii(total_scores, min_r, w*h*area_percentage, 0.8);
+
+        const balls: Matter.Body[] = [];
+        const ball_info: BallInformation[] = [];
+        user_ids.forEach((id) => {
+            balls.push(Bodies.circle(
+                // x position, y position, radius
+                w/2, h/2, radii[id],
+                {
+                    restitution: 0.5,
+                    render: {
+                        fillStyle: CSSStyleDecoration.getPropertyValue("--color-300"),
+                        strokeStyle: CSSStyleDecoration.getPropertyValue("--color-400"),
+                        lineWidth: 10,
+                    },
+                    // info: {
+                    //     user_id: +id,
+                    //     name: users.value[id],
+                    //     total_score: total_scores[id].toFixed(2),
+                    // },
+                }
+            ));
+            ball_info.push({
+                user_id: +id,
+                name: users.value[id],
+                total_score: total_scores[id].toFixed(2),
+            });
+        });
+        const border_color = CSSStyleDecoration.getPropertyValue("--color-500");
+        const ceiling = Bodies.rectangle(
+            // x position, y position, width, height
+            w/2, 0, w, 20, {
+                isStatic: true,
+                render: {
+                    fillStyle: border_color,
                 }
             }
         );
+        const ground = Bodies.rectangle(
+            w/2, h, w, 20, {
+                isStatic: true,
+                render: {
+                    fillStyle: border_color,
+                }
+            }
+        );
+        const left_wall = Bodies.rectangle(
+            0, h/2, 20, h, {
+                isStatic: true,
+                render: {
+                    fillStyle: border_color,
+                }
+            }
+        );
+        const right_wall = Bodies.rectangle(
+            w, h/2, 20, h, {
+                isStatic: true,
+                render: {
+                    fillStyle: border_color,
+                }
+            }
+        );
+        World.add(engine.world, [...balls, ground, left_wall, right_wall, ceiling]);
+
+        Events.on(render, "afterRender", () => {
+            const ctx = render.context;
+            balls.forEach((ball, index, _) => {
+                ctx.save();
+                // 绘制球形高亮
+                let opacity = 0.3;
+                if (selected_ball === null && hovered_ball == null) {
+                    opacity = 1.0
+                } else if (ball === selected_ball || ball === hovered_ball) {
+                    opacity = 1.0;
+                }
+                ctx.globalAlpha = opacity;
+
+                ctx.beginPath();
+                ctx.arc(ball.position.x, ball.position.y, (ball.circleRadius || 0), 0, 2*Math.PI);
+                ctx.fillStyle = ball.render.fillStyle || '';
+                ctx.fill();
+
+                // 绘制球面文字
+                const radius = ball.circleRadius || 0;
+                const text = ball_info[index].name;
+
+                let font_size = radius * 0.8;
+                ctx.font = `${font_size}px 'Times New Roman'`;
+                const width = ctx.measureText(text).width;
+                if (width > radius * 1.7) {
+                    font_size = font_size * (radius * 1.7 / width);
+                    ctx.font = `${font_size}px 'Times New Roman'`;
+                }
+                ctx.fillStyle = '#000000';
+                ctx.textAlign = 'center';
+                ctx.textBaseline = 'middle';
+                ctx.fillText(text, ball.position.x, ball.position.y);
+                ctx.restore();
+            });
+        });
+
+        render.canvas.addEventListener('mouseenter', (e) => {
+            mouse_in_canvas = true;
+        });
+        render.canvas.addEventListener('mouseleave', (e) => {
+            mouse_in_canvas = false;
+        });
+
+        render.canvas.addEventListener('mousemove', (e) => {
+            clearTimeout(mousemove_event_deactivate);
+            const position = {
+                x: e.offsetX,
+                y: e.offsetY,
+            };
+            const target = Query.point(balls, position);
+
+            if (target.length <= 0) {
+                hovered_ball = null;
+                return;
+            }
+            hovered_ball = target[0];
+
+            mousemove_event_deactivate = setTimeout(() => {
+                if (mouse_in_canvas) return;
+                hovered_ball = null;
+            }, 200);
+        });
+        render.canvas.addEventListener('click', (e) => {
+            const position = {
+                x: e.offsetX,
+                y: e.offsetY,
+            };
+            const target = Query.point(balls, position);
+
+            if (target.length <= 0) {
+                selected_ball = null; 
+                do_show_transactions.value = false;
+                return;
+            }
+            if (target[0] === selected_ball) {
+                selected_ball = null;
+                do_show_transactions.value = false;
+                return;
+            }
+            selected_ball = target[0];
+
+            do_show_transactions.value = true;
+            selected_user_id.value = ball_info[balls.indexOf(selected_ball)].user_id;
+
+            clicks.value++;
+        });
     }
 
     // 初始化
@@ -118,7 +301,7 @@
     // 窗口大小变化（含防抖）
     onMounted(() => window.addEventListener('resize', matterJsReInit));
     onUnmounted(() => window.removeEventListener('resize', matterJsReInit));
-    let timeout: number | undefined;
+    let timeout: string | number | NodeJS.Timeout | undefined;
     function matterJsReInit() {
         clearTimeout(timeout);
         timeout = setTimeout(() => {
